3dea2ab9b5eae637d91ef36d03218e6620bc8da2
x404 <kronos2003@gmail.com>
2024-07-29 17:39:40 +0300

    fix: fixed assets path

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   categoryUrl: 'https://139-162-155-38.ip.linodeusercontent.com:4444/api/v1/categories/',
   productsUrl: 'https://139-162-155-38.ip.linodeusercontent.com:4444/api/v1/products/',
-  localCategoryUrl: '/assets/categories.json',
-  localProductsUrl: '/assets/products.json',
+  localCategoryUrl: 'assets/categories.json',
+  localProductsUrl: 'assets/products.json',
 
 
   isDevMode: true,
